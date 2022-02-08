3d939aba0cd052f85be2f5272c21358eb7e1ddaf
Soapiest0821 <74442492+Soapiest0821@users.noreply.github.com>
2022-02-08 16:27:37 +0900

    Update entjs.js

--- entjs.js
+++ entjs.js
@@ -1,63 +1,27 @@
 const zlib = require('zlib')
 const fs = require("fs")
-// console.log(fs.readdirSync("../EntrytoJs").filter(a => a.endsWith(".ent"))[0])
-// fs.writeFileSync("../EntrytoJs", zlib.inflateSync(fs.readFileSync(fs.readdirSync("../EntrytoJs").filter(a => a.endsWith(".ent"))[0], "utf-8")))
-//var output = zlib.inflate(fs.);
 
-const dutkyo = fs.readFileSync("temp/project.json")
+const dutkyo = fs.readFileSync("project temp folder here/temp/project.json")
 const dutjson = JSON.parse(dutkyo)
-if(fs.existsSync("entjs")) {
-  console.log("entjs 폴더를 지우고 다시 시도해주세요")
+if(fs.existsSync("files")) {
+  console.log("files 폴더를 지우고 다시 시도해주세요")
   process.exit()
 } else {
-  fs.mkdirSync("entjs")
+  fs.mkdirSync("files")
 }
-// function code(e) {
-//   fs.writeFileSync('entjs/code ' + new Date().getTime() + '.txt', JSON.stringify(e))
-// }
-// code(dutjson.objects)
+
 for(let i = 0; i < dutjson.objects.length; i++) {
-  fs.mkdirSync('entjs/' + dutjson.objects[i].name
-  .replace(/\n/g, ' ')
-  .replace(/\\/g, ' ')
-  .replace(/\//g, ' ')
-  .replace(/:/g, ' ')
-  .replace(/\*/g, ' ')
-  .replace(/\?/g, ' ')
-  .replace(/"/g, ' ')
-  .replace(/</g, ' ')
-  .replace(/>/g, ' ')
-  .replace(/\|/g, ' ') + ' (' + dutjson.objects[i].id + ')')
-  fs.writeFileSync('entjs/'+dutjson.objects[i].name
-  .replace(/\n/g, ' ')
-  .replace(/\\/g, ' ')
-  .replace(/\//g, ' ')
-  .replace(/:/g, ' ')
-  .replace(/\*/g, ' ')
-  .replace(/\?/g, ' ')
-  .replace(/"/g, ' ')
-  .replace(/</g, ' ')
-  .replace(/>/g, ' ')
-  .replace(/\|/g, ' ') + ' (' + dutjson.objects[i].id + ')/scriptsrc.txt', JSON.stringify(dutjson.objects[i]))
-  fs.writeFileSync('entjs/'+dutjson.objects[i].name
-  .replace(/\n/g, ' ')
-  .replace(/\\/g, ' ')
-  .replace(/\//g, ' ')
-  .replace(/:/g, ' ')
-  .replace(/\*/g, ' ')
-  .replace(/\?/g, ' ')
-  .replace(/"/g, ' ')
-  .replace(/</g, ' ')
-  .replace(/>/g, ' ')
-  .replace(/\|/g, ' ') + ' (' + dutjson.objects[i].id + ')/src.json', JSON.stringify(dutjson.objects[i].script).slice(1, JSON.stringify(dutjson.objects[i].script).length-1).replace(/\\"/g, "\""))
+  fs.mkdirSync('files/' + dutjson.objects[i].name.replace(/(\n|\\|\/|:|\*|\?|"|<|>|\|)/g, ' ') + ' (' + dutjson.objects[i].id + ')')
+  fs.writeFileSync('files/'+dutjson.objects[i].name.replace(/(\n|\\|\/|:|\*|\?|"|<|>|\|)/g, ' ') + ' (' + dutjson.objects[i].id + ')/scriptsrc.txt', JSON.stringify(dutjson.objects[i]))
+  fs.writeFileSync('files/'+dutjson.objects[i].name.replace(/(\n|\\|\/|:|\*|\?|"|<|>|\|)/g, ' ') + ' (' + dutjson.objects[i].id + ')/src.json', JSON.stringify(dutjson.objects[i].script).slice(1, JSON.stringify(dutjson.objects[i].script).length-1).replace(/\\"/g, "\""))
 }
 
 let objects, src, script, codes, blank, bracket
 blank = ""
 bracket = []
-objects = fs.readdirSync("entjs", "utf-8")
-src = "entjs/" + objects[0] + "/"
-script = JSON.parse(fs.readFileSync("entjs/" + objects[0] + "/src.json", "utf-8"))
+objects = fs.readdirSync("files", "utf-8")
+src = "files/" + objects[0] + "/"
+script = JSON.parse(fs.readFileSync("files/" + objects[0] + "/src.json", "utf-8"))
 for(let i=0; i<script.length; i++) {
   console.log(script[i])
   for(let j=0; j<script[i].length; j++) console.log(script[i][j])
@@ -65,18 +29,22 @@ for(let i=0; i<script.length; i++) {
 codes = "const { entry } = require(\"dutkyo\");\n\n"
 
 //for문 쓸 예정
-codes += "entry." + script[0][0]["type"] + "().then(() => {"
+codes += "entry." + script[0][0]["type"] + "(\"" + String.fromCharCode(Number(script[0][0]["params"][1])) + "\").then(() => {"
 bracket.unshift("})")
-
+let rarray = ["i","j","k","l"]
+let rindex = -1
 allcode = function(type) {
-  blank += "  "
   codes += "\n"
   switch(type["type"]) {
     case "repeat_basic":
-      //blank += "  "
       bracket.unshift("}")
-      codes += blank + "for(let i=0; i<" + script[0][1]["params"][0]["params"] + "; i++) {"
-      allcode(type["statements"][0][0])
+      rindex += 1
+      codes += blank + "for(let " + rarray[rindex] + "=0; " + rarray[rindex] + "<" + script[0][1]["params"][0]["params"] + "; " + rarray[rindex] + "++) {"
+      blank += "  "
+      for(let j=0; j<type["statements"][0].length; j++) {
+        allcode(type["statements"][0][j])
+      }
+      rindex -= 1
       blank = blank.slice(0, blank.length - 2)
       codes += "\n" + blank + bracket[0]
       bracket.shift()
@@ -85,11 +53,27 @@ allcode = function(type) {
     case "move_direction":
       codes += blank + "entry.moveDirection(" + type["params"][0]["params"] + ")"
       break
+    case "bounce_wall":
+      codes += blank + "entry.bounceWall()"
+      break
+    case "move_x":
+      codes += blank + "entry.moveX(" + type["params"][0]["params"] + ")"
+      break
+    case "move_y":
+      codes += blank + "entry.moveY(" + type["params"][0]["params"] + ")"
+      break
+    case "move_xy_time":
+      codes += blank + "entry.moveXyTime(" + type["params"][0]["params"] + ", " +  + type["params"][1]["params"] + ", " + type["params"][2]["params"] + ")"
+      break
   }
 }
 
-allcode(script[0][1])
+blank += "  "
+for(let j=1; j<script[0].length; j++) {
+  allcode(script[0][j])
+}
 codes += "\n"
 blank = blank.slice(0, blank.length - 2)
 codes += bracket[0] + "\n" + "\n"
 fs.writeFileSync(src + "script.js", codes)
+console.log("변경했습니다.")
